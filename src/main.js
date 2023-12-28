aedbe0ea02cd1d5ad6549c2cfebb57db3e8ab397
Dylan Drake <dylan.c.drake@gmail.com>
2023-12-27 22:58:34 -0500

    chore: use relative path to root for vite

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import PrimeVue from 'primevue/config';
 
 import { createWebHistory } from 'vue-router'
 import createRouter from './routes.js'
-// import './style.css'
 import './index.css'
-// import 'primevue/resources/themes/lara-light-green/theme.css'
 
 import App from './App.vue'
 
-const router = createRouter(createWebHistory())
+const router = createRouter(createWebHistory());
 
 createApp(App).use(router).use(PrimeVue).mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ["./src/**/*.{html,js,vue}"],
+  content:  ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
