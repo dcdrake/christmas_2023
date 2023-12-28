5f4712da3747633c81ffe32b041294e7dbe216af
Dylan Drake <dylan.c.drake@gmail.com>
2023-12-28 00:00:00 -0500

    chore: make text a bit smaller

--- src/Final.vue
+++ src/Final.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="flex flex-col items-center justify-center h-screen w-full p-8 space-y-3">
-        <span class="lg:text-5xl text-3xl text-slate-600">🎄 Merry Christmas! 🎄</span>
+        <span class="lg:text-5xl text-2xl text-slate-600">🎄 Merry Christmas! 🎄</span>
         <img src="https://media.giphy.com/media/9JrvLb0fnrn7k1ZjhX/giphy.gif" class="rounded-3xl" width="500"
             height="600" />
     </div>
--- src/PageOne.vue
+++ src/PageOne.vue
@@ -9,7 +9,7 @@ const router = useRouter();
 <template>
     <div class="flex flex-row w-full h-screen justify-evenly items-center">
         <div class="flex flex-col space-y-8">
-            <span class="text-5xl text-slate-700">Open present &#x1F436;</span>
+            <span class="lg:text-5xl text-2xl text-slate-700">Open present &#x1F436;</span>
             <button class="border border-blue-600 rounded-xl p-2 bg-blue-600 text-slate-50 text-lg hover:bg-blue-500"
                 @click="router.push({ path: 'page-two' })">
                 <div class="flex flex-row justify-center space-x-2 items-center">
